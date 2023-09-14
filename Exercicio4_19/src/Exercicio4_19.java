af8a6c0d21a49219e702e5ba5f835aa9a35b8629
Souza <mauricio.souza1895@gmail.com>
2023-09-14 11:09:51 -0300

    Testando classe CalculadoraComissao na main

--- Exercicio4_19/src/Exercicio4_19.java
+++ Exercicio4_19/src/Exercicio4_19.java
@@ -3,7 +3,11 @@ public class Exercicio4_19 {
 
 	public static void main(String[] args) 
 	{
+		CalculadoraComissao cc1 = new CalculadoraComissao();
 		
+		cc1.somaVendas();
+		
+		System.out.printf("Total de vendas: %.2f%n", cc1.getTotalVendas());
 	}
 
 }
