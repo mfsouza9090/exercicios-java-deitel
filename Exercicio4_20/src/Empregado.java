a2387fa83eb97adf7297b4b607f0663afb60c954
Souza <mauricio.souza1895@gmail.com>
2023-09-14 11:40:25 -0300

    Criando variáveis de instância, construtor e métodos get e set

--- Exercicio4_20/src/Empregado.java
+++ Exercicio4_20/src/Empregado.java
@@ -1,5 +1,23 @@
 
 public class Empregado 
 {
-
+	private String nome;
+	private int horasTrabalhadas;
+	private double salarioHora;
+	private double salarioBruto;
+	
+	public Empregado (String nome, double salarioHora)
+	{
+		this.nome = nome;
+		this.salarioHora = salarioHora;
+	}
+	
+	public String getNome() { return nome; }
+	
+	public int getHorasTrabalhadas() { return horasTrabalhadas; }
+	
+	public double getSalarioHoras() { return salarioHora; }
+	
+	public double getSalarioBruto() { return salarioBruto; }
+	
 }
