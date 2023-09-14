cad1a151d73def2006eceeb1d3665614e584bbb2
Souza <mauricio.souza1895@gmail.com>
2023-09-14 10:43:40 -0300

    Criando métodos get

--- Exercicio4_19/src/CalculadoraComissao.java
+++ Exercicio4_19/src/CalculadoraComissao.java
@@ -9,6 +9,18 @@ public class CalculadoraComissao
 	private double item3 = 99.95;
 	private double item4 = 350.89;
 	
+	public double getSalarioSemanal() { return salarioSemanal; }
+	
+	public double getTotalVendas() { return totalVendas; }
+	
+	public double getItem1() { return item1; }
+	
+	public double getItem2() { return item2; }
+	
+	public double getItem3() { return item3; }
+	
+	public double getItem4() { return item4; }
+	
 	public double somaVendas()
 	{
 		
