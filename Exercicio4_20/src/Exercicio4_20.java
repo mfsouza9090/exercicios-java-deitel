df1ae588682dd73f0e8306d1a8566aeb672c9bf2
Souza <mauricio.souza1895@gmail.com>
2023-09-14 11:35:37 -0300

    Criando main e classe Empregado; inserindo proposta do exercício

---
+++ Exercicio4_20/src/Exercicio4_20.java
@@ -0,0 +1,15 @@
+/* (Calculador de salários) Desenvolva um aplicativo Java que determina o salário bruto 
+ * de cada um de três empregados. A empresa paga as horas normais pelas primeiras 40 horas 
+ * trabalhadas por cada funcionário e 50% a mais por todas as horas trabalhadas além das 40
+ * horas. Você recebe uma lista de empregados, o número de horas trabalhadas por eles na semana 
+ * passada e o salário-hora de cada um. Seu programa deve aceitar a entrada dessas informações 
+ * para cada empregado e, então, determinar e exibir o salário bruto do empregado.
+ * Utilize a classe Scanner para inserir os dados.*/
+
+public class Exercicio4_20 
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
