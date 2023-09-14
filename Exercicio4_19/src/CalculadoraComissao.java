f5c2d45453dab2fa415ec2a20a4c4d5f26d3818c
Souza <mauricio.souza1895@gmail.com>
2023-09-14 10:40:41 -0300

    Inserindo modificadores de acesso nas variáveis de instância

--- Exercicio4_19/src/CalculadoraComissao.java
+++ Exercicio4_19/src/CalculadoraComissao.java
@@ -1,4 +1,17 @@
+import java.util.Scanner;
 
-public class CalculadoraComissao {
-
+public class CalculadoraComissao 
+{
+	private double salarioSemanal = 200.00;
+	private double totalVendas = 0;
+	private double item1 = 239.99;
+	private double item2 = 129.75;
+	private double item3 = 99.95;
+	private double item4 = 350.89;
+	
+	public double somaVendas()
+	{
+		
+		return 0.0;
+	}
 }
