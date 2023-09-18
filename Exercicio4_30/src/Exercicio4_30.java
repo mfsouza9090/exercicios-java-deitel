3ef69e401d224c89c0c90dbbaad10cdf52b9f2de
Souza <mauricio.souza1895@gmail.com>
2023-09-18 13:15:22 -0300

    Adicionando novo exercício e proposta

---
+++ Exercicio4_30/src/Exercicio4_30.java
@@ -0,0 +1,16 @@
+/* (Palíndromos) Um palíndromo é uma sequência de caracteres que é lida 
+ * da esquerda para a direita ou da direita para a esquerda. Por exemplo, 
+ * cada um dos seguintes inteiros de cinco dígitos é um palíndromo: 12321, 
+ * 55555, 45554 e 11611. Escreva um aplicativo que lê em um inteiro de cinco 
+ * dígitos e determina se ele é ou não um palíndromo. Se o número não for de 
+ * cinco dígitos, exiba uma mensagem de erro e permita que o usuário insira 
+ * um novo valor. */
+
+public class Exercicio4_30 {
+
+	public static void main(String[] args) 
+	{
+
+	}
+
+}
