f7c134bed65fad5f1b994fefc31f6bc40c369aa9
Souza <mauricio.souza1895@gmail.com>
2023-10-02 12:24:17 -0300

    Adicionando novo exercício e proposta

---
+++ Exercicio4_35/src/Exercicio4_35.java
@@ -0,0 +1,13 @@
+/* (Lados de um triângulo) Escreva um aplicativo que lê três 
+ * valores diferentes de zero inseridos pelo usuário, determina 
+ * e imprime se eles poderiam representar os lados de um 
+ * triângulo. */ 
+
+public class Exercicio4_35 
+{
+	public static void main(String[] args) 
+	{
+
+	}
+
+}
