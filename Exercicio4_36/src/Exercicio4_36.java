1d7779ccb4b0e044c3bc9be54655708c1416f4d3
Souza <mauricio.souza1895@gmail.com>
2023-10-02 14:27:02 -0300

    Implementando instruções de seleção na main

--- Exercicio4_36/src/Exercicio4_36.java
+++ Exercicio4_36/src/Exercicio4_36.java
@@ -2,10 +2,62 @@
  * inteiros diferentes de zero, determina e imprime se eles poderiam
  * representar os lados de um triângulo direito. */
 
+import java.util.Scanner;
+
 public class Exercicio4_36 
 {
 	public static void main(String[] args) 
 	{
+		Scanner input = new Scanner(System.in);
+		int lado1 = 0;
+		int lado2 = 0;
+		int lado3 = 0;
 		
+		System.out.print("Insira o primeiro lado do triângulo: ");
+		lado1 = input.nextInt();
+		
+		while (lado1 <= 0)
+		{
+			System.out.println("Input inválido. Insira um inteiro positivo");
+			System.out.print("Insira o primeiro lado do triângulo: ");
+			lado1 = input.nextInt();
+		}
+		
+		System.out.print("Insira o segundo lado do triângulo: ");
+		lado2 = input.nextInt();
+		
+		while (lado2 <= 0)
+		{
+			System.out.println("Input inválido. Insira um inteiro positivo");
+			System.out.print("Insira o segundo lado do triângulo: ");
+			lado2 = input.nextInt();
+		}
+		
+		System.out.print("Insira o terceiro lado do triângulo: ");
+		lado3 = input.nextInt();
+		
+		while (lado3 <= 0)
+		{
+			System.out.println("Input inválido. Insira um inteiro positivo");
+			System.out.print("Insira o terceiro lado do triângulo: ");
+			lado3 = input.nextInt();
+		}
+		
+		if ((lado1 * lado1) == ((lado2 * lado2) + (lado3 * lado3)))
+		{
+			System.out.print("Os inteiros inseridos podem representar os lados de um triângulo retângulo");
+		}
+		else if ((lado2 * lado2) == ((lado1 * lado1) + (lado3 * lado3)))
+		{
+			System.out.print("Os inteiros inseridos podem representar os lados de um triângulo retângulo");
+		}
+		else if ((lado3 * lado3) == ((lado1 * lado1) + (lado2 * lado2)))
+		{
+			System.out.print("Os inteiros inseridos podem representar os lados de um triângulo retângulo");
+		}
+		else
+		{
+			System.out.print("Os inteiros inseridos não podem representar os lados de um triângulo retângulo");
+		}
 	}
 }
