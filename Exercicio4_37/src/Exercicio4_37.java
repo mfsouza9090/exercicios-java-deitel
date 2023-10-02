8edc2c2e5e7e520c427f8083536847007217381d
Souza <mauricio.souza1895@gmail.com>
2023-10-02 14:48:50 -0300

    Adicionando novo exercício e proposta

---
+++ Exercicio4_37/src/Exercicio4_37.java
@@ -0,0 +1,25 @@
+/* (Fatorial) O fatorial de um inteiro não negativo n é escrito como n! 
+ * (pronuncia-se “n fatorial”) e é definido como segue:
+ * 
+ * n! = n · (n – 1) · (n – 2) · ... · 1 (para valores de n maiores ou iguais a 1)
+ * 
+ * e
+ * 
+ * n! = 1 (para n = 0)
+ * 
+ * por exemplo, 5! = 5 · 4 · 3 · 2 · 1, o que dá 120.
+ * 
+ * a) Escreva um aplicativo que lê um inteiro não negativo, calcula e imprime 
+ * seu fatorial.
+ * b) Escreva um aplicativo que estima o valor da constante matemática. Permita ao 
+ * usuário inserir o número de termos a calcular.
+ * c) Escreva um aplicativo que computa o valor de e ao quadrado. Permita ao usuário 
+ * inserir o número de termos a calcular. */
+
+public class Exercicio4_37 
+{
+	public static void main(String[] args) 
+	{
+
+	}
+}
