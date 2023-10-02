13e706769d9dcf537f8ed810b5027074f43cbcc5
Souza <mauricio.souza1895@gmail.com>
2023-10-02 13:50:44 -0300

    Adicionando novo exercício e proposta

---
+++ Exercicio4_36/src/Exercicio4_36.java
@@ -0,0 +1,11 @@
+/* (Lados de um triângulo direito) Escreva um aplicativo que lê três 
+ * inteiros diferentes de zero, determina e imprime se eles poderiam
+ * representar os lados de um triângulo direito. */
+
+public class Exercicio4_36 
+{
+	public static void main(String[] args) 
+	{
+		
+	}
+}
