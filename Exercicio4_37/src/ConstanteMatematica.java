044f30cc838dee1176e805bd624586802169d815
Souza <mauricio.souza1895@gmail.com>
2023-10-04 13:14:28 -0300

    Refatorando main (criando menu e inserindo instruções de seleção

--- Exercicio4_37/src/ConstanteMatematica.java
+++ Exercicio4_37/src/ConstanteMatematica.java
@@ -1,4 +1,18 @@
 
-public class ConstanteMatematica {
-
+public class ConstanteMatematica 
+{
+	public static double calculaConstante(int termos)
+	{
+		double e = 1;
+		int contador = 1;
+		
+		while (contador <= termos)
+		{
+			int fatorial = Fatorial.retornaFatorial(contador);
+			e += 1 / (double) fatorial;
+			++contador;
+		}
+		
+		return e;
+	}
 }
--- Exercicio4_37/src/Exercicio4_37.java
+++ Exercicio4_37/src/Exercicio4_37.java
@@ -25,9 +25,54 @@ import java.util.Scanner;
 
 public class Exercicio4_37 
 {
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
 		Scanner input = new Scanner(System.in);
+		int menuInput = 0;
+		
+		System.out.println("Menu"
+				+ "\n1 - Fatorial (Subexercício A)"
+				+ "\n2 - Constante matemática de e (Subexercício B)"
+				+ "\n3 - Valor de e elevado a x (Subexercício C)"
+				+ "\n4 - Sair\n");
+		
+		menuInput = input.nextInt();
+		
+		if (menuInput == 1)
+		{
+			int fatorialDe = 0;
+			
+			System.out.print("Insira um inteiro positivo: ");
+			fatorialDe = input.nextInt();
+			
+			while (fatorialDe <= 0)
+			{
+				System.out.println("Input inválido");
+				System.out.print("Insira um inteiro positivo: ");
+				fatorialDe = input.nextInt();
+			}
+			
+			System.out.printf("%d! = %d%n", fatorialDe, Fatorial.retornaFatorial(fatorialDe));
+		}
+		else if (menuInput == 2)
+		{
+			int termos = 0;
+			
+			System.out.print("Insira o número de termos: ");
+			termos = input.nextInt();
+			
+			while (termos <= 0)
+			{
+				System.out.println("Input inválido");
+				System.out.print("Insira o número de termos: ");
+				termos = input.nextInt();
+			}
+			
+			System.out.printf("Valor da constante matemática e: %.2f", 
+					ConstanteMatematica.calculaConstante(termos));
+		}
+		
+		/* Scanner input = new Scanner(System.in);
 		int numeroUsuario = 0;
 		
 		System.out.print("Insira um inteiro positivo: ");
@@ -40,7 +85,7 @@ public class Exercicio4_37
 			numeroUsuario = input.nextInt();
 		}
 		
-		System.out.print(Fatorial.retornaFatorial(numeroUsuario));
+		System.out.print(Fatorial.retornaFatorial(numeroUsuario)); */
 		
 		input.close();
 	}
