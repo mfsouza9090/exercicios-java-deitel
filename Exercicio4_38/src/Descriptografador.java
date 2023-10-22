2e9a702801ffdcda675b61c3cd8c4301a58c7f5e
Souza <mauricio.souza1895@gmail.com>
2023-10-22 20:36:37 -0300

    Concluindo classe Descriptografador

--- Exercicio4_38/src/Descriptografador.java
+++ Exercicio4_38/src/Descriptografador.java
@@ -1,19 +1,34 @@
-
 public class Descriptografador 
 {
-	public static int descriptografador (int dadoCriptografado)
+	public static int descriptografador(int dadoCriptografado)
 	{
 		int contador = 0;
 		int digitosAlternados = 0;
+		int multiploDeDez = 1000;
+		int dadoOriginal = dadoCriptografado;
+		int dadoDescriptografado = 0;
 		
 		while (contador < 2)
 		{
-			int resto = dadoCriptografado % 100;
+			int resto = dadoOriginal % 100;
 			digitosAlternados = digitosAlternados * 100 + resto;
-			dadoCriptografado /= 100;
+			dadoOriginal /= 100;
 			++contador;
 		}
 		
-		return digitosAlternados;
+		contador = 0;
+		
+		while (contador < 4)
+		{
+			int digitoUnico;
+			
+			digitoUnico = digitosAlternados / multiploDeDez;
+			digitosAlternados %= multiploDeDez;
+			dadoDescriptografado += (digitoUnico - 7 + 10) * multiploDeDez;
+			multiploDeDez /= 10;
+			++contador;
+		} 
+		
+		return dadoDescriptografado;
 	}
 }
