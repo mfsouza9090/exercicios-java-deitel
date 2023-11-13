69b502fa39639d4c2f281930de9927bca19faa92
Souza <mauricio.souza1895@gmail.com>
2023-11-13 17:12:53 -0300

    Adicionando exercício 5_13

---
+++ Exercicio5_13/src/Exercicio5_13.java
@@ -0,0 +1,27 @@
+/* (Fatoriais) Fatoriais costumam ser utilizados em problemas
+ * de probabilidade. O fatorial de um inteiro positivo n 
+ * (escrito como n! e pronunciado como “fatorial de n”) é igual
+ * ao produto dos números inteiros positivos de 1 a n. Escreva 
+ * um aplicativo que calcula os fatoriais de 1 a 20. Utilize o 
+ * tipo long. Exiba os resultados em formato tabular. Que 
+ * dificuldade poderia impedir você de calcular o fatorial de 100? */ 
+
+public class Exercicio5_13 {
+
+	public static void main(String[] args) 
+	{
+		for (int contador = 1; contador <= 20; contador++)
+		{
+			long fatorial = contador;
+			int decrementador = contador;
+			
+			while (decrementador > 1)
+			{
+				fatorial = fatorial * (decrementador - 1);
+				--decrementador;
+			}
+			
+			System.out.printf("%d! = %d%n", contador, fatorial);
+		}
+	}
+}
