fb456eaafa02c3f8396fdbf097839edf49dfe64c
Souza <mauricio.souza1895@gmail.com>
2023-11-13 14:16:37 -0300

    Removendo loop for e inserindo loop while na main

--- Exercicio5_11/src/Exercicio5_11.java
+++ Exercicio5_11/src/Exercicio5_11.java
@@ -10,12 +10,24 @@ public class Exercicio5_11 {
 	{
 		Scanner input = new Scanner(System.in);
 		int menorValor = 0;
+		int contador;
 		
 		System.out.println("Insira um número inteiro. Este número determinará\n"
 				       + "a quantidade de entradas que você deverá digitar em\n"
 				       + "seguida:");
+		// a expressão abaixo se justifica pois a entrada do primeiro inteiro 
+		// se dará fora do escopo do loop, que deverá iterar x vezes - 1
+		contador = input.nextInt() - 1;
 		
-		for (int contador = input.nextInt(); contador > 0; contador--)
+		// as três linhas a seguir garantem que o valor da variável
+		// menorValor seja igual ao valor do primeiro inteiro digitado
+		// pelo usuário antes do início do loop. sem elas, o menor
+		// valor seria sempre 0 
+		System.out.println();
+		System.out.println("Insira um número inteiro:");
+		menorValor = input.nextInt();
+		
+		while (contador > 0)
 		{
 			System.out.println();
 			System.out.println("Insira um número inteiro:");
@@ -23,7 +35,10 @@ public class Exercicio5_11 {
 			
 			if (inteiroUsuario < menorValor)
 				menorValor = inteiroUsuario;
+			
+			contador--;
 		}
+		
 		System.out.println();
 		System.out.printf("Menor valor digitado: %d%n", menorValor);
 		
