1ee9120c1f7b090c12df7b7d73f4b7f55243a6a7
Souza <mauricio.souza1895@gmail.com>
2023-11-13 13:58:56 -0300

    Removendo loop do...while e reinserindo loop for na main

--- Exercicio5_11/src/Exercicio5_11.java
+++ Exercicio5_11/src/Exercicio5_11.java
@@ -15,27 +15,18 @@ public class Exercicio5_11 {
 				       + "a quantidade de entradas que você deverá digitar em\n"
 				       + "seguida:");
 		
-		for (int contador = input.nextInt(), copiaContador = input.nextInt(); contador > 0; contador--)
+		for (int contador = input.nextInt(); contador > 0; contador--)
 		{
 			System.out.println();
 			System.out.println("Insira um número inteiro:");
 			int inteiroUsuario = input.nextInt();
 			
-			// assegura que na primeira iteração o valor de
-			// menorValor não seja 0, mas o valor do primeiro 
-			// inteiro inserido pelo usuário (caso contrário,
-			// o valor de menorValor sempre será 0, independente
-			// dos valores inseridos pelo usuário)
-			if (contador == copiaContador)
-				menorValor = input.nextInt();
-			else if (inteiroUsuario < menorValor)
+			if (inteiroUsuario < menorValor)
 				menorValor = inteiroUsuario;
 		}
-		
 		System.out.println();
 		System.out.printf("Menor valor digitado: %d%n", menorValor);
 		
 		input.close();
 	}
-
 }
