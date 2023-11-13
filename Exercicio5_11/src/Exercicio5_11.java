1e02ce3c167b8cb7d364f523ae9772af5a99145a
Souza <mauricio.souza1895@gmail.com>
2023-11-13 12:06:41 -0300

    Inserindo instrução if no loop for na main

--- Exercicio5_11/src/Exercicio5_11.java
+++ Exercicio5_11/src/Exercicio5_11.java
@@ -9,15 +9,27 @@ public class Exercicio5_11 {
 	public static void main(String[] args) 
 	{
 		Scanner input = new Scanner(System.in);
+		int inteiroUsuario;
+		int menorValor;
 		
-		System.out.print("Insira um número inteiro. Este número especificará\n"
-				       + "a quantidade de entradas que você deverá digitar em "
-				       + "seguida");
+		System.out.println("Insira um número inteiro. Este número determinará\n"
+				       + "a quantidade de entradas que você deverá digitar em\n"
+				       + "seguida:");
 		
-		for (int contador = input.nextInt();contador > 0; contador--)
+		for (int contador = input.nextInt(); contador > 0; contador--)
 		{
-			System.out.println("teste");
+			System.out.println();
+			System.out.println("Insira um número inteiro:");
+			inteiroUsuario = input.nextInt();
+			menorValor = inteiroUsuario;
+			
+			if (inteiroUsuario < menorValor)
+				menorValor = inteiroUsuario;
 		}
+		
+		System.out.println();
+		System.out.printf("Menor valor: %d%n", menorValor);
+		input.close();
 	}
 
 }
