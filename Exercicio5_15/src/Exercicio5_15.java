327d1b1b8ccb97b64db0b0fd24ffb408a23dd530
Souza <mauricio.souza1895@gmail.com>
2023-11-14 14:13:25 -0300

    Adicionando novo exercício

---
+++ Exercicio5_15/src/Exercicio5_15.java
@@ -0,0 +1,30 @@
+/* (Programa para impressão de triângulos) Escreva um aplicativo que 
+ * exibe os seguintes padrões separadamente, um embaixo do outro. 
+ * Utilize loops for para gerar os padrões. Todos os asteriscos (*) 
+ * devem ser impressos por uma única instrução na forma System.out.print('*'); 
+ * o que faz com que os asteriscos sejam impressos lado a lado. Uma 
+ * instrução na forma System.out.println(); pode ser utilizada para mover-se
+ * para a próxima linha. Uma instrução na forma System.out.print(' ');
+ * pode ser utilizada para exibir um espaço para os últimos dois padrões. 
+ * Não deve haver outras instruções de saída no programa. [Dica: os dois 
+ * últimos padrões requerem que cada linha inicie com um número adequado 
+ * de espaços em branco.] */
+
+public class Exercicio5_15 
+{
+	public static void main(String[] args) 
+	{
+		// a)
+		for (int contador = 1; contador <= 10; contador++)
+		{
+			int contadorAux = contador;
+			
+			while (contadorAux > 0)
+			{
+				System.out.print("*");
+				contadorAux--;
+			}
+			System.out.println();
+		}
+	}
+}
