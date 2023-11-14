505cc1eb2fad52c6ce7b68ea7314d1ce9b438c71
Souza <mauricio.souza1895@gmail.com>
2023-11-14 13:46:59 -0300

    Inserindo instrução switch para atualizar variável rate

--- Exercicio5_14/src/Exercicio5_14.java
+++ Exercicio5_14/src/Exercicio5_14.java
@@ -7,18 +7,44 @@ public class Exercicio5_14
 {
 	public static void main(String[] args) 
 	{
-		// código inicial fornecido pelo livro
 		double amount;
 		double principal = 1000.0;
 		double rate = 0.05;
 		
 		System.out.printf("%s%20s %n", "Year", "Amount on deposit");
 		
-		for (int year = 1; year <= 10; ++year)
+		for(int counter = 1; counter <= 6; counter++)
 		{
-			amount = principal * Math.pow(1.0 + rate, year);
+			for (int year = 1; year <= 10; ++year)
+			{
+				amount = principal * Math.pow(1.0 + rate, year);
+				
+				System.out.printf("%4d%,20.2f%n", year, amount);
+			}
 			
-			System.out.printf("%4d%,20.2f%n", year, amount);
+			System.out.println();
+			
+			// se usassse += para somar 0.01 ao valor da variável
+			// rate a cada iteração do loop for externo, os resultados
+			// eventualmente seriam imprecisos. por isso optei pela 
+			// instrução de seleção múltipla
+			switch (counter)
+			{
+			case 1:
+				rate = 0.06;
+				break;
+			case 2:
+				rate = 0.07;
+				break;
+			case 3:
+				rate = 0.08;
+				break;
+			case 4: 
+				rate = 0.09;
+				break;
+			case 5:
+				rate = 0.1;
+			}
 		}
 	}
 }
