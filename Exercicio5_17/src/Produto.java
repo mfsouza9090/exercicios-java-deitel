5d0e07ce572b8093438c0e0b9332d370537bb27a
Souza <mauricio.souza1895@gmail.com>
2023-11-15 12:26:42 -0300

    Criando construtor e métodos get e set da classe Produto

--- Exercicio5_17/src/Produto.java
+++ Exercicio5_17/src/Produto.java
@@ -1,4 +1,22 @@
 
 public class Produto {
-
+	private double preco;
+	private double qtdVendida;
+	
+	public Produto(double preco) {
+		this.preco = preco;
+	}
+	
+	public double getPreco() {
+		return preco;
+	}
+	
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+	public void setQtdVendida(double qtdVendida) {
+		this.qtdVendida = qtdVendida;
+	}
+	
 }
