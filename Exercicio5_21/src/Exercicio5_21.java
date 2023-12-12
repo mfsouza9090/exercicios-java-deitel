52de3e962587b4a1f2b755b1d5744795214a6b94
Souza <mauricio.souza1895@gmail.com>
2023-12-12 18:11:34 -0300

    Adicionando comentários e concluíndo exercício

--- Exercicio5_21/src/Exercicio5_21.java
+++ Exercicio5_21/src/Exercicio5_21.java
@@ -10,18 +10,35 @@
  * Esse é um método de computação de “força bruta”. Você aprenderá 
  * nos cursos de ciência da computação mais avançados que para muitos 
  * problemas interessantes não há uma abordagem algorítmica conhecida 
- * além do uso de força bruta absoluta. */
+ * além do uso de força bruta absoluta. */ 
+
+import java.lang.Math;
 
 public class Exercicio5_21 {
 
 	public static void main(String[] args) {
-		for (int side1 = 1; side1 <= 500; side1++);{
-			for (int side2 = 1; side2 <= 500; side2++){
-				for (int hypotenuse = 1; hypotenuse <= 500; hypotenuse++){
-					
+		System.out.println("+----------------------------+");
+		System.out.printf("|     %19s    |%n", "Ternos Pitagóricos");
+		System.out.println("+----------------------------+");
+		System.out.printf("| %5s | %5s | %10s |%n", "side1", "side2", "hypotenuse");
+		System.out.println("+----------------------------+");
+		
+		// as três variáveis de controle são do tipo double para
+		// que seja possível usar Math.pow
+		for (double side1 = 1; side1 <=500; side1++) {
+			for (double side2 = 1; side2 <= 500; side2++) {
+				for (double hypotenuse = 1; hypotenuse <= 500; hypotenuse++) {
+					// se a soma dos quadrados dos catetos for igual ao quadrado
+					// da hipotenusa, mostrar o conjunto na tablea
+					if (Math.pow(side1, 2) + Math.pow(side2, 2) == Math.pow(hypotenuse, 2)) {
+						System.out.printf("|  %3.0f  | %5.0f | %10.0f |", side1, side2, hypotenuse);
+						System.out.println();
+					}
 				}
 			}
 		}
+		
+		System.out.println("+----------------------------+");
 	}
 
 }
