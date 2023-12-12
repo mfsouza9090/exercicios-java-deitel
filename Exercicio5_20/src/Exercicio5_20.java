40a82b6369bafbea261da1b4c81a5def462ca5c5
Souza <mauricio.souza1895@gmail.com>
2023-12-12 16:26:17 -0300

    Implementando instrução if para determinar primeira vez que pi aparece

--- Exercicio5_20/src/Exercicio5_20.java
+++ Exercicio5_20/src/Exercicio5_20.java
@@ -9,18 +9,23 @@ public class Exercicio5_20 {
 		double perimetro = 4;
 		int diametro = 3;
 		double pi = 4;
+		boolean sinal = true;
 		int marca = 0;
-		String piStr = "";
 		
+		// cabeçalho da tabela
 		System.out.println(" ------------------------------------");
-		System.out.printf("|      %-6s |      %-15s |", "Termo", "Valor de pi");
-		System.out.println();
+		System.out.printf("|      %-6s |      %-15s |%n", "Termo", "Valor de pi");
 		System.out.println(" ------------------------------------");
 		
+		// calcula os termos da série
 		for (int i = 1; i <= 200000; i++) {
+			// se i for ímpar, subtraia de pi o quocioente da
+			// divisão entre perímetro e diâmetro
 			if (i % 2 != 0){
 				pi -= perimetro / diametro;
 			}
+			// caso contrário, some a pi o quociente da divisão
+			// entre perímetro e diâmetro
 			else {
 				pi += perimetro / diametro;
 			}
@@ -28,16 +33,18 @@ public class Exercicio5_20 {
 			System.out.printf("|      %6d | %20.14f |", i, pi);
 			System.out.println();
 			
-			piStr = String.format("%.5f", pi);
+			// converte pi em uma string representando um double
+			// com cinco casas decimais
+			String piStr = String.format("%.5f", pi);
+			boolean eIgual = piStr.equals("3,14159");
 			
-			System.out.println();
-			System.out.println(piStr);
-			System.out.println();
-			
-			if (piStr == "3,14159"){
+			// armazena a iteração em que o valor de pi foi
+			// 3,14159 pela primeira vez
+			if (sinal && eIgual){
 				marca = i;
+				sinal = false;
 			}
-			
+
 			diametro += 2;
 		}
 		
