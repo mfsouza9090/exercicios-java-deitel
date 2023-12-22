76ed2e9974b1cef30f84355d0b979ea9f3ce15a0
Souza <mauricio.souza1895@gmail.com>
2023-12-22 10:02:01 -0300

    Implementando os dois primeiros loops for na main

--- Exercicio5_22/src/Exercicio5_22.java
+++ Exercicio5_22/src/Exercicio5_22.java
@@ -8,80 +8,32 @@ public class Exercicio5_22 {
 	public static void main(String[] args) {
 		for (int contador = 1; contador <= 10; contador++)
 		{
-			int contadorAux = contador;
-			
-			while (contadorAux > 0)
-			{
-				System.out.print("*");
-				contadorAux--;
-			}
-			System.out.println();
-		}
-		
-		System.out.println();
-		
-		// b)
-		for (int contador = 10; contador > 0; contador--)
-		{
-			int contadorAux = contador;
-			
-			while (contadorAux > 0)
-			{
-				System.out.print("*");
-				contadorAux--;
-			}
-			System.out.println();
-		}
-		
-		System.out.println();
-		
-		// c)
-		for (int contador = 10; contador > 0; contador--)
-		{
 			int asteriscos = contador;
-			// o número de espaços em branco é calculado
-			// a partir do contador do loop for externo:
-			// 0 na primeira iteração, 1 na segunda, 2
-			// na terceira e assim por diante
-			int espacos = 9 - (contador - 1);
-			
-			while (espacos > 0)
-			{
-				System.out.print(" ");
-				espacos--;
-			}
 			
 			while (asteriscos > 0)
 			{
 				System.out.print("*");
 				asteriscos--;
 			}
-		
-			System.out.println();
+			
+			for (int contador1 = 10; contador1 > 0; contador1--) {
+				int asteriscos1 = contador1;
+				int espacos1 = 9 - (contador1 - 1);
+				
+				while (espacos1 > 0) {
+					System.out.print(" ");
+					espacos1--;
+				}
+				
+				while (asteriscos1 > 0) {
+					System.out.print("*");
+					asteriscos1--;
+				}
+				System.out.println();
+			}
 		}
 		
-		System.out.println();
 		
-		// d)
-		for (int contador = 1; contador <= 10; contador++)
-		{
-			int asteriscos = contador;
-			int espacos = 10 - contador;
-			
-			while (espacos > 0)
-			{
-				System.out.print(" ");
-				espacos--;
-			}
-			
-			while (asteriscos > 0)
-			{
-				System.out.print("*");
-				asteriscos--;
-			}
-		
-			System.out.println();
-		}
 	}
 
 }
