7a5e7b52ab500f032d3fce8e1bd964a309f08bdd
Souza <mauricio.souza1895@gmail.com>
2024-01-09 17:38:44 -0300

    Adicionando novo exercício c/ enunciado

---
+++ Exercicio5_25/src/Exercicio5_25.java
@@ -0,0 +1,13 @@
+/* (Programa de impressão de losangos modificado) Modifique o aplicativo 
+ * que você escreveu na Questão 5.24 para ler um número ímpar no intervalo 
+ * 1 a 19 para especificar o número de linhas no losango. Seu programa então 
+ * deve exibir um losango do tamanho apropriado. */
+
+public class Exercicio5_25 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
