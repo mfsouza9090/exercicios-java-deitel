176f9f1c2960133334a7e226d158d33f17230aec
Souza <mauricio.souza1895@gmail.com>
2024-01-09 15:20:56 -0300

    Adicionando novo exercício c/ enunciado

---
+++ Exercicio5_24/src/Exercicio5_24.java
@@ -0,0 +1,15 @@
+/* (Programa de impressão de losangos) Escreva um aplicativo que 
+ * imprime a seguinte forma de um losango. Você pode utilizar 
+ * instruções de saída que imprimem um único asterisco (*), um único 
+ * espaço ou um único caractere de nova linha. Maximize sua utilização 
+ * de repetição (com instruções for aninhadas) e minimize o número de 
+ * instruções de saída */
+
+public class Exercicio5_24 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
